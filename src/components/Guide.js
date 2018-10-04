4c46cac04818cce8adb5a662471401b0e2cf5421
Raphael Martins <ram@hellofresh.com>
2018-10-04 15:12:16 +0200

    Extracting transform calculation to helper file

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -19,80 +19,8 @@ const Guide = styled.div`
   border-radius: ${props => props.rounded}px;
 
   transform: ${props => {
-    const {
-      targetTop,
-      targetRight,
-      targetBottom,
-      targetLeft,
-      windowWidth,
-      windowHeight,
-      helperWidth,
-      helperHeight,
-      helperPosition,
-      padding,
-    } = props
-
-    const available = {
-      left: targetLeft,
-      right: windowWidth - targetRight,
-      top: targetTop,
-      bottom: windowHeight - targetBottom,
-    }
-
-    const couldPositionAt = position => {
-      return (
-        available[position] >
-        (hx.isHoriz(position)
-          ? helperWidth + padding * 2
-          : helperHeight + padding * 2)
-      )
-    }
-
-    const autoPosition = coords => {
-      const positionsOrder = hx.bestPositionOf(available)
-      for (let j = 0; j < positionsOrder.length; j++) {
-        if (couldPositionAt(positionsOrder[j])) {
-          return coords[positionsOrder[j]]
-        }
-      }
-      return coords.center
-    }
-
-    const pos = helperPosition => {
-      const hX = hx.isOutsideX(targetLeft + helperWidth, windowWidth)
-        ? hx.isOutsideX(targetRight + padding, windowWidth)
-          ? targetRight - helperWidth
-          : targetRight - helperWidth + padding
-        : targetLeft - padding
-      const x = hX > padding ? hX : padding
-      const hY = hx.isOutsideY(targetTop + helperHeight, windowHeight)
-        ? hx.isOutsideY(targetBottom + padding, windowHeight)
-          ? targetBottom - helperHeight
-          : targetBottom - helperHeight + padding
-        : targetTop - padding
-      const y = hY > padding ? hY : padding
-      const coords = {
-        top: [x, targetTop - helperHeight - padding * 2],
-        right: [targetRight + padding * 2, y],
-        bottom: [x, targetBottom + padding * 2],
-        left: [targetLeft - helperWidth - padding * 2, y],
-        center: [
-          windowWidth / 2 - helperWidth / 2,
-          windowHeight / 2 - helperHeight / 2,
-        ],
-      }
-      if (helperPosition === 'center' || couldPositionAt(helperPosition)) {
-        return coords[helperPosition]
-      }
-      return autoPosition(coords)
-    }
-
-    const p = pos(helperPosition)
-    console.log(`p[0]:${p[0]}`)
-    console.log(`targetLeft:${targetLeft}`)
-    console.log(`${p[0] - targetLeft}`)
-    return `translate(${p[0]}px, ${p[1]}px)`
+    const translate = hx.getTransform(props)
+    return `translate(${translate[0]}px, ${translate[1]}px)`
   }};
 `
-
 export default Guide
--- src/helpers.js
+++ src/helpers.js
@@ -37,3 +37,76 @@ export const bestPositionOf = positions => {
     .sort((a, b) => b.value - a.value)
     .map(p => p.position)
 }
+
+export const getTransform = props => {
+  const {
+    targetTop,
+    targetRight,
+    targetBottom,
+    targetLeft,
+    windowWidth,
+    windowHeight,
+    helperWidth,
+    helperHeight,
+    helperPosition,
+    padding,
+  } = props
+
+  const available = {
+    left: targetLeft,
+    right: windowWidth - targetRight,
+    top: targetTop,
+    bottom: windowHeight - targetBottom,
+  }
+
+  const couldPositionAt = position => {
+    return (
+      available[position] >
+      (isHoriz(position)
+        ? helperWidth + padding * 2
+        : helperHeight + padding * 2)
+    )
+  }
+
+  const autoPosition = coords => {
+    const positionsOrder = bestPositionOf(available)
+    for (let j = 0; j < positionsOrder.length; j++) {
+      if (couldPositionAt(positionsOrder[j])) {
+        return coords[positionsOrder[j]]
+      }
+    }
+    return coords.center
+  }
+
+  const pos = helperPosition => {
+    const hX = isOutsideX(targetLeft + helperWidth, windowWidth)
+      ? isOutsideX(targetRight + padding, windowWidth)
+        ? targetRight - helperWidth
+        : targetRight - helperWidth + padding
+      : targetLeft - padding
+    const x = hX > padding ? hX : padding
+    const hY = isOutsideY(targetTop + helperHeight, windowHeight)
+      ? isOutsideY(targetBottom + padding, windowHeight)
+        ? targetBottom - helperHeight
+        : targetBottom - helperHeight + padding
+      : targetTop - padding
+    const y = hY > padding ? hY : padding
+    const coords = {
+      top: [x, targetTop - helperHeight - padding * 2],
+      right: [targetRight + padding * 2, y],
+      bottom: [x, targetBottom + padding * 2],
+      left: [targetLeft - helperWidth - padding * 2, y],
+      center: [
+        windowWidth / 2 - helperWidth / 2,
+        windowHeight / 2 - helperHeight / 2,
+      ],
+    }
+    if (helperPosition === 'center' || couldPositionAt(helperPosition)) {
+      return coords[helperPosition]
+    }
+    return autoPosition(coords)
+  }
+
+  const p = pos(helperPosition)
+  return p
+}
