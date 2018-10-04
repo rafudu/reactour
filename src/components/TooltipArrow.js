2bca404af5d8a654cce71791316cdd67adaab5ad
Raphael Martins <ram@hellofresh.com>
2018-10-04 16:10:46 +0200

    Remove useless code

--- src/components/TooltipArrow.js
+++ src/components/TooltipArrow.js
@@ -19,20 +19,5 @@ const TooltipArrow = styled.span`
     return `translate(${props.targetLeft - translate[0] + 5}px, 0)`
   }};
 `
-// const TooltipArrow = styled.span`
-//   position: absolute;
-//   background-color: var(--reactour-accent);
-//   height: 1.875em;
-//   line-height: 2;
-//   padding-left: 0.8125em;
-//   padding-right: 0.8125em;
-//   font-size: 1em;
-//   border-radius: 1.625em;
-//   color: white;
-//   text-align: center;
-//   box-shadow: 0 0.25em 0.5em rgba(0, 0, 0, 0.3);
-//   top: -0.8125em;
-//   left: -0.8125em;
-// `
 
 export default TooltipArrow
